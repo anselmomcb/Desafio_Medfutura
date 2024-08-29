c3fcda653d544485eea58c38be5c9bc137989b12
Anselmo <anselmomendes1998@gmail.com>
2024-08-29 16:34:08 -0300

    inicializando o projeto com spring

---
+++ src/test/java/com/desafio/medfutura/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.medfutura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
